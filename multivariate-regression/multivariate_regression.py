4075b5e074a895894652c8d3dcff980612555be6
Kevin Kronk <kjkronk@ncsu.edu>
2019-02-28 12:05:58 -0500

    multi gradient descent module.

--- multivariate-regression/multivariate_regression.py
+++ multivariate-regression/multivariate_regression.py
@@ -13,61 +13,6 @@ housing_data = np.concatenate([np.ones((data_length, 1)), X_norm], axis=1)
 multi_J = computeCostMulti(housing_features, housing_prices, theta=np.array([[0], [0], [0]]))
 
 
-def gradientDescentMulti(X, y, theta, alpha, num_iters):
-    """
-    Performs gradient descent to learn theta.
-    Updates theta by taking num_iters gradient steps with learning rate alpha.
-
-    Parameters
-    ----------
-    X : array_like
-        The dataset of shape (m x n+1).
-
-    y : array_like
-        A vector of shape (m, ) for the values at a given data point.
-
-    theta : array_like
-        The linear regression parameters. A vector of shape (n+1, )
-
-    alpha : float
-        The learning rate for gradient descent.
-
-    num_iters : int
-        The number of iterations to run gradient descent.
-
-    Returns
-    -------
-    theta : array_like
-        The learned linear regression parameters. A vector of shape (n+1, ).
-
-    J_history : list
-        A python list for the values of the cost function after each iteration.
-
-    Instructions
-    ------------
-    Perform a single gradient step on the parameter vector theta.
-
-    While debugging, it can be useful to print out the values of
-    the cost function (computeCost) and gradient here.
-    """
-    # Initialize some useful values
-    m = y.shape[0]  # number of training examples
-
-    # make a copy of theta, which will be updated by gradient descent
-    theta = theta.copy()
-
-    J_history = []
-
-    for i in range(num_iters):
-        temp_J = computeCostMulti(X, y, theta)
-        J_history.append(temp_J)
-        delta = (1/m)*((np.dot(X, theta))-y)*X
-        delta2 = np.array(delta.sum(axis=0))
-        delta3 = delta2.reshape(-1, 1)
-        theta = (theta - (alpha*delta3))
-        i += 1
-    return theta, J_history
-
 
 theta = np.array([[0], [0], [0]])
 alpha_list = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 1.1, 1.2]
