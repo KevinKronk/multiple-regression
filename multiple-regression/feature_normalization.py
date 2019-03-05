fixed feature normalization for pandas.

multiple-regression/feature_normalization.py
@@ -1,7 +1,8 @@
 # Feature Normalization
+import numpy as np
 
 
-def featureNormalize(X):
+def feature_normalize(x):
     """
     Normalizes the features in X. returns a normalized version of X where
     the mean value of each feature is 0 and the standard deviation
@@ -34,14 +35,14 @@ def featureNormalize(X):
     You might find the 'np.mean' and 'np.std' functions useful.
     """
     # You need to set these values correctly
-    X_norm = X.copy()
-    mu = np.zeros(X.shape[1])
-    sigma = np.zeros(X.shape[1])
+    x_norm = x.copy()
+    mu = np.zeros(x.shape[1])
+    sigma = np.zeros(x.shape[1])
 
-    features = X.shape[1]
+    features = x.shape[1]
     for feature in range(features):
-        mu[feature] = np.mean(X[:, feature])
-        sigma[feature] = np.std(X[:, feature])
-        X_norm[:, feature] = (X[:, feature] - mu[feature]) / sigma[feature]
-    return X_norm, mu, sigma
+        mu[feature] = np.mean(x.iloc[:, feature])
+        sigma[feature] = np.std(x.iloc[:, feature])
+        x_norm.iloc[:, feature] = (x.iloc[:, feature] - mu[feature]) / sigma[feature]
+    return x_norm, mu, sigma
 

multiple-regression/multiple_regression.py
@@ -1,6 +1,7 @@
 # import numpy as np
 # import pandas as pd
 from load_data import load_data
+from feature_normalization import feature_normalize
 
 
 # Linear regression with multiple variables - predicting housing prices
@@ -9,13 +10,17 @@ from load_data import load_data
 # Load Data
 
 filename = 'housing_data.txt'
-x, y, size = load_data(filename)
+housing_data, housing_prices, size = load_data(filename)
 
-'''
-X_norm, mu, sigma = featureNormalize(housing_data)
+
+# Feature Normalization
+
+x_norm, mu, sigma = feature_normalize(housing_data)
 
 print('Computed mean:', mu)
 print('Computed standard deviation:', sigma)
+
+'''
 
 # now we add the intercept term to housing_data
 housing_data = np.concatenate([np.ones((data_length, 1)), X_norm], axis=1)