fixed cost, gradient, prediction modules.

multiple-regression/multi_cost.py
@@ -1,10 +1,8 @@
+import numpy as np
 # Cost Function for multiple variables
-housing_features = housing_data[:, :3]
-housing_prices = housing_data[:, 3]
-housing_prices = housing_prices[:, None]
-data_length = housing_prices.size
 
-def computeCostMulti(X, y, theta):
+
+def multi_cost(x, y, theta):
     """
     Compute cost for linear regression with multiple variables.
     Computes the cost of using theta as the parameter for linear regression to fit the data points in X and y.
@@ -29,9 +27,7 @@ def computeCostMulti(X, y, theta):
     ------------
     Compute the cost of a particular choice of theta. You should set J to the cost.
     """
-    m = y.shape[0]  # number of training examples
+    size = y.shape[0]  # number of training examples
 
-    first = (np.dot(X, theta) - y).T
-    second = (np.dot(X, theta) - y)
-    cost = np.sum((1/(2*m))*(np.dot(first, second)))
+    cost = np.sum((1 / (2 * size)) * (((np.dot(x, theta.T)) - y) ** 2))
     return cost

multiple-regression/multi_gradient.py
@@ -1,11 +1,15 @@
-def gradientDescentMulti(X, y, theta, alpha, num_iters):
+import numpy as np
+from multi_cost import multi_cost
+
+
+def gradient_descent(x, y, theta, alpha, iters):
     """
     Performs gradient descent to learn theta.
     Updates theta by taking num_iters gradient steps with learning rate alpha.
 
     Parameters
     ----------
-    X : array_like
+    x : array_like
         The dataset of shape (m x n+1).
 
     y : array_like
@@ -36,19 +40,24 @@ def gradientDescentMulti(X, y, theta, alpha, num_iters):
     the cost function (computeCost) and gradient here.
     """
     # Initialize some useful values
-    m = y.shape[0]  # number of training examples
+    size = y.shape[0]  # number of training examples
 
     # make a copy of theta, which will be updated by gradient descent
-    theta = theta.copy()
+    parameters = x.shape[1]
+    temp = np.zeros((1, parameters))
+    cost_history = np.zeros(iters)
 
-    J_history = []
+    for i in range(iters):
 
-    for i in range(num_iters):
-        temp_J = computeCostMulti(X, y, theta)
-        J_history.append(temp_J)
-        delta = (1/m)*((np.dot(X, theta))-y)*X
-        delta2 = np.array(delta.sum(axis=0))
-        delta3 = delta2.reshape(-1, 1)
-        theta = (theta - (alpha*delta3))
-        i += 1
-    return theta, J_history
+        error = (1 / size) * ((np.dot(x, theta.T)) - y)
+
+        for parameter in range(parameters):
+            delta = error * x[:, [parameter]]
+
+            temp[0, parameter] = theta[0, parameter] - (alpha * delta.sum())
+
+        # delta2 = delta.sum(axis=1, keepdims=True)
+        theta = temp
+        cost_history[i] = multi_cost(x, y, theta)  # misses the last theta update
+
+    return theta, cost_history

multiple-regression/multiple_regression.py
@@ -1,6 +1,10 @@
 import numpy as np
-# import pandas as pd
+import pandas as pd
 from load_data import load_data
+import matplotlib.pyplot as plt
+from multi_cost import multi_cost
+from multi_gradient import gradient_descent
+from prediction import predict_price
 
 
 # Linear regression with multiple variables - predicting housing prices
@@ -10,57 +14,61 @@ from load_data import load_data
 
 filename = 'housing_data.txt'
 housing_data = load_data(filename)
-print(housing_data)
+
+alpha = 0.01
+iterations = 1000
 
 # Feature Normalization
+means = housing_data.mean()
+stds = housing_data.std()
 
 housing_data = (housing_data - housing_data.mean()) / housing_data.std()
-print(housing_data)
-
-# Feature Normalization
-
-# x_norm, mu, sigma = feature_normalize(housing_data, size)
-#
-# print('Computed mean:', mu)
-# print('Computed standard deviation:', sigma)
-
-# now we add the intercept term to housing_data
 
 
-intercept = pd.DataFrame(np.ones(size, 1))
-x_norm.join(intercept)
-#  x_norm = np.concatenate([np.ones((size, 1)), x_norm], axis=1)
-# adds row of 1s to first column of X_norm
+# Inserting Intercept parameter
 
+housing_data.insert(0, 'Ones', 1)
+
+
+# Converting Pandas dataframe to necessary numpy arrays
+
+cols = housing_data.shape[1]
+
+x = housing_data.iloc[:, 0:cols-1]
+y = housing_data.iloc[:, cols-1:cols]  # why do I need the :cols
+
+x = x.values
+y = y.values
+theta = np.array([[0, 0, 0]])
+
+
+cost = multi_cost(x, y, theta)
+
+
+theta, cost_history = gradient_descent(x, y, theta, alpha, iterations)
+print(theta)
+print(cost_history)
+
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(list(range(iterations)), cost_history, 'r')
+plt.xlabel("Iterations")
+plt.ylabel("Cost")
+plt.title("Cost History in Epochs")
+plt.show()
+
+
+# Prediction
+
+pprice = predict_price(1650, 3, means, stds, theta)
+print(pprice)
 
 '''
 
-multi_J = computeCostMulti(housing_features, housing_prices, theta=np.array([[0], [0], [0]]))
-
-
-
-theta = np.array([[0], [0], [0]])
-alpha_list = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 1.1, 1.2]
-num_iters = 50
-plt.figure(2)
-
-for alpha2 in alpha_list:
-    new_theta2, J_history2 = gradientDescentMulti(housing_features, housing_prices, theta, alpha2, num_iters)
-
-    plt.plot(np.arange(len(J_history2)), J_history2, lw=2)
-    plt.xlabel('Number of iterations')
-    plt.ylabel('Cost J')
-
-# Plot the convergence graph
-plt.show(2)
-
-alpha = 1.2
-new_theta2, J_history2 = gradientDescentMulti(housing_features, housing_prices, theta, alpha, num_iters)
-print(new_theta2)
 
 # make a prediction for a 1650 sq-ft, 3 br house
-f1 = (1650-mu[0])/sigma[0]
-f2 = (3-mu[1])/sigma[1]
+f1 = 1650
+f2 = 3
 f3 = (np.dot([1, f1, f2], new_theta2))
 predict3 = float(f3*sigma[2])+mu[2]
 print("Predicted price of a 1650 sq-ft, 3 bed room house: ${:.0f}".format(predict3))